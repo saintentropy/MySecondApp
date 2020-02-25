a725b46b35c80dd6abce9dd32a579c672c5cd8c8
Craig Long <craig.long@autodesk.com>
2020-02-25 16:29:54 -0500

    Add configuration

--- MySecondApp/Program.cs
+++ MySecondApp/Program.cs
@@ -1,12 +1,41 @@
-﻿using System;
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
 
 namespace MySecondApp
 {
     class Program
     {
+        public static IReadOnlyDictionary<string, string> DefaultConfigurationStrings { get; } =
+            new Dictionary<string, string>()
+            {
+                ["Profile:UserName"] = Environment.UserName,
+                ["AppConfiguration:MainWindow:Height"] = "10",
+                ["AppConfiguration:MainWindow:Width"] = "100"
+            };
+
+        public static IConfiguration Configuration { get; set; }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            ConfigurationBuilder configurationBuilder = new ConfigurationBuilder();
+            configurationBuilder.AddInMemoryCollection(DefaultConfigurationStrings);
+            //configurationBuilder.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+
+            Configuration = configurationBuilder.Build();
+            Console.SetWindowSize(Int32.Parse(Configuration["AppConfiguration:MainWindow:Width"]), (Int32.Parse(Configuration["AppConfiguration:MainWindow:Height"])));
+            Console.WriteLine($"Hello {Configuration["Profile:UserName"]}");
+            Console.WriteLine($"{Configuration["AppConfiguration:MainWindow:Height"]}");
+
+            while (true)
+            {
+                var w = Int32.Parse(Configuration["AppConfiguration:MainWindow:Width"] ?? "100");
+                var h = Int32.Parse(Configuration["AppConfiguration:MainWindow:Height"] ?? "10");
+                Console.SetWindowSize(w, h);
+            }
+
+
         }
     }
 }
